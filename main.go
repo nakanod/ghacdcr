c043209eb64dc430e86734a98061f0a7ddf23a6f
Nakano Daisuke <mail@nakanod.net>
2023-12-05 09:55:58 +0900

    test

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world on %s %s\n", os.Getenv("TARGET"), os.Getenv("SHA"))
+	fmt.Fprintf(w, "Hello, world %s %s\n", os.Getenv("TARGET"), os.Getenv("SHA"))
 }
 
 func main() {
